b1e5369516519276f89aedcd2107dea5b7f25fec
kaniha <kani9hasuresh621@gmail.com>
2025-06-09 16:22:28 +0530

    index.html

---
+++ asset/pyhton/demo.py
@@ -0,0 +1,27 @@
+a = 10
+b = 20
+c=30
+if a > b and a > c:
+    print("a is greater")
+elif b>a and b > c:
+    print(" b is greater")
+else:
+    print("c is greater")        
+for i in range(2,6,2):
+    print(i)
+def add():
+    print(10+20)
+add()        
+list =[1,2,3,4,5];
+for i in list:
+    print(i)
+
+    tuple = (6,7,8,9,10);
+for i in tuple:
+    print(i)
+print(type(tuple))
+dictionary = {"name":"kaniha","password":"@1324"}
+print(dictionary["name"])
+print(type(dictionary)) 
+for key in dictionary:
+    print(dictionary[key])  
